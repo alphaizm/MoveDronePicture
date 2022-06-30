223077fa36b672e564574fcbe39062a53408c1ab
alphaizm <aquaprialpha@gmail.com>
2022-06-30 23:57:41 +0900

    処理 ・リストビュー向けのViewModelクラスを追加 ・Json定義クラスを追加

---
+++ MoveDronePicture/MoveDronePicture/CtrlPicData.cs
@@ -0,0 +1,77 @@
+﻿// INotifyPropertyChanged notifCies the View of property changes, so that Bindings are updated.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Data;
+
+namespace MoveDronePicture
+{
+    /// <summary>
+    /// 写真ファイル通知用クラスの管理
+    /// </summary>
+    public sealed class cCtrlPicData
+    {
+        public ObservableCollection<cPicData> PicData { get; }
+
+        public cCtrlPicData() {
+            PicData = new ObservableCollection<cPicData>();
+            // 複数スレッドからコレクション操作できるようにする
+            BindingOperations.EnableCollectionSynchronization(PicData, new object());
+        }
+
+        public void AddPicData(string[] ary_pathes_) {
+            PicData.Clear();
+            foreach ( var path in ary_pathes_ ) {
+                PicData.Add(new cPicData(path));
+            }
+        }
+    }
+
+    /// <summary>
+    /// 写真ファイル通知用クラス
+    /// </summary>
+    public sealed class cPicData : INotifyPropertyChanged
+    {
+        private string _Path;
+        private string _FileName;
+        private double _Lat;
+        private double _Lon;
+        private double _Height;
+
+        public cPicData(string str_path_) {
+            _Path = str_path_;
+            _Lat = 0;
+            _Lon = 0;
+            _Height = 0;
+
+            var start = _Path.LastIndexOf(@"\") + 1;
+            _FileName = _Path.Substring(start, _Path.Length - start);
+
+            //  todo    上記から必要なデータを読みだして各プロパティに入れる
+        }
+
+        public string FileName {
+            get { return _FileName; }
+        }
+
+        public string Lat {
+            get { return _Lat.ToString(); }
+        }
+
+        public string Lon {
+            get { return _Lon.ToString(); }
+        }
+
+        public string Height {
+            get { return _Height.ToString(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string property_name_) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property_name_));
+        }
+    }
+}
---
+++ MoveDronePicture/MoveDronePicture/JsonData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveDronePicture
+{
+    public class cJsonBase
+    {
+        public string DirSrc { get; set; }
+        public string DirDst { get; set; }
+        public List<cBlock> LstBlocks { get; set; }
+
+        public cJsonBase() {
+            DirSrc = "";
+            DirDst = "";
+            LstBlocks = new List<cBlock>();
+        }
+
+        public cJsonBase(string str_dir_src_, string str_dir_dst_, List<cBlock> lst_blocks_) {
+            DirSrc = str_dir_src_;
+            DirDst = str_dir_dst_;
+            LstBlocks = lst_blocks_;
+        }
+
+    }
+
+    public class cBlock
+    {
+        public string Name { get; set; }
+        public Dictionary<string, double> DicFolders { get; set; }
+        public List<cPoint> LstPoints { get; set; }
+
+        public cBlock() {
+            Name = "";
+            DicFolders = new Dictionary<string, double>();
+            LstPoints = new List<cPoint>();
+        }
+
+        public cBlock(string str_name_, Dictionary<string, double> dic_folders_, List<cPoint> lst_points_) {
+            Name = str_name_;
+            DicFolders = dic_folders_;
+            LstPoints = lst_points_;
+        }
+
+    }
+
+    public class cPoint
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+
+        public cPoint() {
+            Lat = 0;
+            Lon = 0;
+        }
+
+        public cPoint(double lat_, double lon_) {
+            Lat = lat_;
+            Lon = lon_;
+        }
+    }
+}
