0e5c8a65c6863e83f5fb64a836791157481cb9f6
alphaizm <aquaprialpha@gmail.com>
2022-07-04 00:25:14 +0900

    IMGプロパティから緯度・経度取得処理を追加

--- MoveDronePicture/MoveDronePicture/CtrlPicData.cs
+++ MoveDronePicture/MoveDronePicture/CtrlPicData.cs
@@ -7,6 +7,7 @@ using System.ComponentModel;
 using System.Text;
 using System.Windows.Data;
 using System.IO;
+using System.Drawing;
 
 namespace MoveDronePicture
 {
@@ -42,6 +43,11 @@ namespace MoveDronePicture
         private double _Lon;
         private double _Height;
 
+        const int ID_LAT = 0x0002;
+        const int ID_LON = 0x0004;
+        const int ID_HEIGHT = 0x0006;
+        const int ID_DATE = 0x9004;
+
         public cPicData(string str_path_) {
             _Path = str_path_;
             _Lat = 0;
@@ -49,7 +55,25 @@ namespace MoveDronePicture
             _Height = 0;
 
             _FileName = Path.GetFileName(str_path_);
-            FileSystemInfo.
+            //var file = await Window.Storage.strageFile.GetFileFromPathAsynck(str_path_);
+            Bitmap bmp = new Bitmap(str_path_);
+            foreach (var prop in bmp.PropertyItems) {
+                switch (prop.Id) {
+                    case ID_DATE:
+                        var str_time = Encoding.UTF8.GetString(prop.Value);
+                        break;
+                    case ID_LAT:
+                        _Lat = GetDecLatLon(prop.Value);
+                        break;
+                    case ID_LON:
+                        _Lon = GetDecLatLon(prop.Value);
+                        break;
+                    case ID_HEIGHT:
+                        break;
+                    default:
+                        break;
+                }
+            }
             //  todo    上記から必要なデータを読みだして各プロパティに入れる
         }
 
@@ -67,6 +91,25 @@ namespace MoveDronePicture
 
         public string Height {
             get { return _Height.ToString(); }
+        }
+
+        private double GetDecLatLon(byte[] ary_value_) {
+            uint deg_numerator = BitConverter.ToUInt32(ary_value_, 0);
+            //uint deg_denominator = BitConverter.ToUInt32(ary_value_, 4);
+
+            uint min_numerator = BitConverter.ToUInt32(ary_value_, 8);
+            //uint min_denominator = BitConverter.ToUInt32(ary_value_, 12);
+
+            uint sec_numerator = BitConverter.ToUInt32(ary_value_, 16);
+            uint sec_denominator = BitConverter.ToUInt32(ary_value_, 20);
+
+            double sec = 0;
+            if (0 != sec_denominator) {
+                sec = (double)sec_numerator / (double)sec_denominator;
+            }
+
+            double ret = deg_numerator + min_numerator / 60 + sec / 3600;
+            return ret;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
