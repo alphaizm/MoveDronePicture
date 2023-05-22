b44f48c46033019b7ef1241bb2c5bc26e6852521
Atsushi Nakada <duke_abuji@hotmail.com>
2023-05-23 01:49:10 +0900

    [GcpEditor] ・出力用の辞書データを追加 ・出漁の辞書データに追加、削除

--- MoveDronePicture/MoveDronePicture/CtrlGcpItem.cs
+++ MoveDronePicture/MoveDronePicture/CtrlGcpItem.cs
@@ -30,7 +30,7 @@ namespace MoveDronePicture
 
 	public sealed class cGcpItem : INotifyPropertyChanged
 	{
-		private string _Name;
+		private string _PointName;
 		private double _Lat;
 		private double _Lon;
 		private double _Height;
@@ -39,7 +39,7 @@ namespace MoveDronePicture
 
 		public cGcpItem() {
 			// 入力値はデバッグ値
-			_Name = "Test";
+			_PointName = "Test";
 			_Lat = 1.0;
 			_Lon = 2.0;
 			_Height = 3.0;
@@ -48,7 +48,7 @@ namespace MoveDronePicture
 		}
 
 		public cGcpItem(string name_, double lat_, double lon_, int x_, int y_) {
-			_Name = name_;
+			_PointName = name_;
 			_Lat = lat_;
 			_Lon = lon_;
 			_Height = 0;
@@ -56,7 +56,71 @@ namespace MoveDronePicture
 			_ImgY = y_;
 		}
 
-		public string Name { get { return _Name; } }
+		public string Name { get { return _PointName; } }
+
+		/// <summary>
+		/// WGS84座標の緯度：latitude
+		/// 例）140.85908710956599
+		/// </summary>
+		public double Lat { get { return _Lon; } }
+
+		/// <summary>
+		/// WGS84座標の経度：longitude
+		/// 例）38.191641339096201
+		/// </summary>
+		public double Lon { get { return _Lon; } }
+
+		/// <summary>
+		/// WGS84座標の高さ
+		/// 例）12.8
+		/// </summary>
+		public double Height { get { return _Height; } }
+
+		/// <summary>
+		/// 選択画像のX座標
+		/// </summary>
+		public int ImgX { get { return _ImgX; } }
+
+		/// <summary>
+		/// 選択画像のY座標
+		/// </summary>
+		public int ImgY { get { return _ImgY; } }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged(string property_name_) {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property_name_));
+		}
+	}
+
+	public class cGcpRegistered
+	{
+		private string _ImgName;
+		private double _Lat;
+		private double _Lon;
+		private double _Height;
+		private int _ImgX;
+		private int _ImgY;
+
+		public cGcpRegistered() {
+			// 入力値はデバッグ値
+			_ImgName = "Test";
+			_Lat = 1.0;
+			_Lon = 2.0;
+			_Height = 3.0;
+			_ImgX = 5;
+			_ImgY = 6;
+		}
+
+		public cGcpRegistered(string name_, double lat_, double lon_, double hgt_, int x_, int y_) {
+			_ImgName = name_;
+			_Lat = lat_;
+			_Lon = lon_;
+			_Height = hgt_;
+			_ImgX = x_;
+			_ImgY = y_;
+		}
+
+		public string ImgName { get { return _ImgName; } }
 
 		/// <summary>
 		/// WGS84座標の緯度：latitude
@@ -103,10 +167,5 @@ namespace MoveDronePicture
 		/// 選択画像のY座標
 		/// </summary>
 		public int ImgY { get { return _ImgY; } }
-
-		public event PropertyChangedEventHandler PropertyChanged;
-		private void NotifyPropertyChanged(string property_name_) {
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property_name_));
-		}
 	}
 }
